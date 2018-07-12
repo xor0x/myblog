44d80d74c22ff90ed9f0f5007ba4561db7ce01e0
root <root@localhost.localdomain>
2018-07-12 11:25:58 +0300

    Change Admin

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from .models import Portfolio
 
 
-
+#Admin Dashboard add content
+admin.site.site_title='Klondike-X'
+admin.site.site_header = 'Klonike-X Admin Dashboard'
+admin.site.index_title = 'Welcome to Klondike-X Dashboard'
 
 
 
