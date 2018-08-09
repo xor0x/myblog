4bbdd963bd9c07e720a1ffa7895f759de47a79ff
xor0x <alex.misyuk1@gmail.com>
2018-08-09 10:04:34 -0400

    Change design

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,6 @@ from django.shortcuts import render, get_object_or_404
 from .models import BlogNews, Post, Category, SiteSettings
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
-def list_of_post_category(request, category_slug):
-    category = get_object_or_404(Category, slug=category_slug)
-    post = Post.objects.filter(category=category)
-    return render(request, 'blog/post/category.html', {'post':post, 'category':category,})
 
 def home(request):
     try:
@@ -16,13 +12,45 @@ def home(request):
         news = BlogNews.objects.get()
     except BlogNews.DoesNotExist:
         news = None
+    categorys = Category.objects.all()
     posts = Post.objects.get_queryset().order_by('id')
     paginator = Paginator(posts,1)
     page = request.GET.get('page')
     posts = paginator.get_page(page)
-    return render(request, 'blog/home.html', {'news':news,'posts':posts,'site_settings':site_settings,})
+    ctx = {
+    'news':news,
+    'posts':posts,
+    'site_settings':site_settings,
+    'categorys':categorys,
+    }
+    url = 'blog/home.html'
+    return render(request, url, ctx)
 
 
 def detail(request,post_id):
+    categorys = Category.objects.all()
+    try:
+        news = BlogNews.objects.get()
+    except BlogNews.DoesNotExist:
+        news = None
     post = get_object_or_404(Post, pk=post_id)
-    return render(request, 'blog/detail.html',{'post':post})
+    ctx = {
+    'post':post,
+    'news':news,
+    'categorys':categorys,
+    }
+    url = 'blog/detail.html'
+    return render(request, url, ctx)
+
+
+def list_of_post_category(request, category_slug):
+    categorys = Category.objects.all()
+    category = get_object_or_404(Category, slug=category_slug)
+    post = Post.objects.filter(category=category)
+    ctx = {
+    'post':post,
+    'category':category,
+    'categorys':categorys,
+    }
+    url = 'blog/post/category.html'
+    return render(request, url, ctx)
