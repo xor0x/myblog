a959a35d6db72f3bab9e3857ed50a8aeaf58d84f
xor0x <alex.misyuk1@gmail.com>
2018-08-18 15:48:12 +0300

    Add new

--- account/urls.py
+++ account/urls.py
@@ -1,1 +1,8 @@
-urlpatterns = []
+from django.urls import path
+from account import views
+
+urlpatterns = [
+        path('signup', views.signup, name='signup'),
+        path('login', views.login, name='login'),
+        path('logout', views.logout, name='logout'),
+]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+from .forms import SignUpForm
+from django.contrib.auth import authenticate
 
-# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'accounts/signup.html', {'form': form})
+
+
+def login(request):
+    if request.method == 'POST':
+        user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            ctx = {
+                'error':'Username or Password incorrect.'
+            }
+            return render(request, 'accounts/login.html', ctx)
+    else:
+        return render(request,'accounts/login.html')
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
+
+    return render(request, 'accounts/signup.html')
