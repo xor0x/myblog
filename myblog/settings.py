4e6fd005da7974adcd016422e3e6bb81fc996603
xor0x <alex.misyuk1@gmail.com>
2018-08-18 16:03:37 +0300

    Add new

--- myblog/settings.py
+++ myblog/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'portfolio.apps.PortfolioConfig',
     'blog.apps.BlogConfig',
+    'account.apps.AccountConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
